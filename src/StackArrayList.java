8c4b30cfd8e18f97e747a22acf01e3d4587c9630
bloomfieldfong <bloomfieldf24@gmail.com>
2017-02-01 11:32:35 -0600

    f

--- src/StackArrayList.java
+++ src/StackArrayList.java
@@ -1,14 +1,28 @@
 import java.util.ArrayList;
 
-public class StackArrayList<E>
- implements Stack<E>
-{
+public class StackArrayList<E>implements iPila<E>{
 	protected ArrayList<E> data;
 
-	public StackArrayList()public StackArrayList()
+	public StackArrayList(){
 	// post: constructs a new, empty stack
 	{
 		data = new ArrayList<E>();
+		
+	}
+	@Override
+	public void push(E algo) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public E pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
