1f68c26434f15f8553d2827750e4dee1d0f2886f
bloomfieldfong <bloomfieldf24@gmail.com>
2017-02-01 11:06:02 -0600

    Agrege metodos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,28 @@
 
 public class Calculadora {
+	
+	
+	public Calculadora(){
+		
+	}
+	
 
+	public int suma(int a, int b){
+		return 0;
+	}
+	
+	public int resta(int a, int b){
+		return 0;
+	}
+	
+	public int multiplicacion(int a, int b){
+		return 0;
+	}
+	
+	public int division(int a, int b){
+		return 0;
+	}
+	
+	
+	
 }
