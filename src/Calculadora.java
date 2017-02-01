a6f576e41a3436954aba043627d43442bee2d5e8
bloomfieldfong <bloomfieldf24@gmail.com>
2017-02-01 11:07:04 -0600

    ...

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -8,19 +8,24 @@ public class Calculadora {
 	
 
 	public int suma(int a, int b){
-		return 0;
+		
+		int c = a+b;
+		return c;
 	}
 	
 	public int resta(int a, int b){
-		return 0;
+		int c = a-b;
+		return c;
 	}
 	
 	public int multiplicacion(int a, int b){
-		return 0;
+		int c = a*b;
+		return c;
 	}
 	
 	public int division(int a, int b){
-		return 0;
+		int c = a/b;
+		return c;
 	}
 	
 	
