ca8781e045311fac2aad6997e0f06cfc3ae25d12
bloomfieldfong <bloomfieldf24@gmail.com>
2017-02-01 11:15:32 -0600

    Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,10 @@
 
+/**
+ * @author Rodrigo Juarez Jui 16073
+ * @author Michelle Bloomfield Fong 16803
+ *
+ */
+
 public class Calculadora {
 	
 	
@@ -7,22 +13,42 @@ public class Calculadora {
 	}
 	
 
+	/**
+	 * @param a
+	 * @param b
+	 * @return la suma de los enteros
+	 */
 	public int suma(int a, int b){
 		
 		int c = a+b;
 		return c;
 	}
 	
+	/**
+	 * @param a
+	 * @param b
+	 * @return la resta de los enteros
+	 */
 	public int resta(int a, int b){
 		int c = a-b;
 		return c;
 	}
 	
+	/**
+	 * @param a
+	 * @param b
+	 * @return la multiplicacion de los enteros
+	 */
 	public int multiplicacion(int a, int b){
 		int c = a*b;
 		return c;
 	}
 	
+	/**
+	 * @param a
+	 * @param b
+	 * @return la division de los enteros
+	 */
 	public int division(int a, int b){
 		int c = a/b;
 		return c;
